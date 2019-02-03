0cbd5c1346f5aee9b903f3d1e77c7dde960ee570
Krzysztof1274 <k.domoslawski@tfiagro.pl>
2019-02-03 11:18:53 +0100

    dodatnie metody onClick

--- app/src/main/java/com/example/dkr1tfi/loginpanel/MainActivity.java
+++ app/src/main/java/com/example/dkr1tfi/loginpanel/MainActivity.java
@@ -2,22 +2,27 @@ package com.example.dkr1tfi.loginpanel;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void zaloguj(View view){
+
+        EditText loginInput = (EditText) findViewById(R.id.loginInput);
+        EditText passwordInput = (EditText) findViewById(R.id.passwordInput);
+        Log.i("Info", loginInput.getText().toString());
+        Log.i("Info", passwordInput.getText().toString());
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button zalogujButton = (Button) findViewById(R.id.loginButton);
-        zalogujButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
 
-            }
-        });
     }
 }
